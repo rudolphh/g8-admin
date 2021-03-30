f17e024f2a07a5d94127095e84ccd5776f7e4f59
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-03-30 08:37:19 -0700

    update

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const app = require('./app')
 const express = require('express')
 const cors = require('cors');
 
+const iplocate = require('node-iplocate');
+const publicIP = require('public-ip');
+const fetch = require('node-fetch');
+const {LocalStorage} = require('node-localstorage');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const port = process.env.PORT || 4000;
 
 app.use(cors());
@@ -48,7 +55,88 @@ app.get('/top-sports', (req, res) => {
   }).limit(3);
 });
 
+app.get('/weather', (err, response) => {
+
+  publicIP.v4()
+  .then(ip=>{
+      iplocate(ip)
+      .then((results)=>{
+          let city=results.city
+          let country=results.country_code
+          //console.log(results.city+","+results.country_code)
+
+          let url=`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=e89bbf79ee4c5ca05e9ba54a351cb0b5`
+          fetch(
+              url,
+              {method:'GET'})
+          .then(res=>res.json())
+          .then(json=>{
+              //console.log(json)
+              response.send(json)
+          })
+          .catch(err=>{throw err})
+      })
+  })
+
+});
+
 
 const server = app.listen(port, () => {
   console.log('App started on ' + port);
 });
+
+let io = socketIO(server,{
+  cors: {
+    origins: ['http://localhost:4200']
+  }
+})
+
+io.sockets.on('connection',(socket)=>{
+  console.log('a user connected (socket)')
+  var city;
+
+  let list=socket.client.conn.server.clients
+  let users=Object.keys(list)
+
+
+  //consuming my events with labels 
+  socket.on('nick',(nick)=>{
+       socket.nickname=nick
+       
+       //console.log(users)
+       socket.emit('userList',users)
+  })
+
+
+  socket.on('chat',(data)=>{
+      
+      publicIP.v4()
+       .then(ip=>{
+           iplocate(ip)
+            .then((results)=>{
+                var city=JSON.stringify(results.city,null,2)
+                localstorage.setItem('userLocal',city)
+                //console.log('city: ',city)
+            })
+       })
+
+      if(socket.nickname){
+          let nickname=socket.nickname
+          let curDate = new Date().toLocaleTimeString();
+          let payload={
+              message:data,
+              nick:nickname,
+              date:curDate,
+              location:localstorage.getItem('userLocal')
+              //location:city
+          }
+
+          console.log('payload: ', payload)
+          //socket.emit('userList',users)
+          //socket.emit('chat',payload)
+          socket.broadcast.emit('chat',payload)
+      }else{
+         //do nothing
+      }
+  })
+})
